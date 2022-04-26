c690f7fc9e42eb97b0743a979642cb4fc653606a
wagnam <szma001@student.kristiania.no>
2022-04-26 20:18:12 +0200

    Added the process

--- portfolio-site/src/views/SkumCase.vue
+++ portfolio-site/src/views/SkumCase.vue
@@ -1,9 +1,11 @@
 <template>
   <skum-header></skum-header>
-  <article class="container">
+  <main class="container">
+      <section>
+          <!-- Section for about this project -->
     <div class="row mt-5">
       <div class="col-md-6 col-12">
-        <h5 class="about-project">ABOUT THE PROJECT</h5>
+        <h5 class="about-project mb-3 fs-6">ABOUT THE PROJECT</h5>
         <p class="about-projext-txt">
           <strong>Skum</strong> is a <strong>mobile application </strong>that I
           and two other students created as part of our interaction design exam.
@@ -25,9 +27,24 @@
           <p class="col-6 col-md-4">Analysis</p>
           <p class="col-6 col-md-4">User-scenario</p>
         </div>
+        <div class="row mt-3">
+            <p class="col" style="font-weight: 700">Date</p>
+            <p>2020 - 8 Weeks</p>
+        </div>
       </div>
     </div>
-  </article>
+    </section>
+    <!-- End of section -->
+
+    <!-- The process section -->
+    <article class="the-process-container mt-5">
+        <h3 class="fs-4" style="font-weight: 700">THE PROCESS</h3>
+        <img class="img-fluid mt-5" src="../assets/images/UUU-model.png" alt="UUU model">
+
+    </article>
+
+  </main>
+
 </template>
 
 <script>
@@ -43,12 +60,13 @@ export default {
 </script>
 
 <style scoped>
-.test {
-  display: inline-block !important;
-}
 
 .container {
   font-family: Montserrat, sans-serif;
+}
+
+.fs-4{
+    color: #0d0d0d;
 }
 .about-project {
   color: #0d0d0d;
