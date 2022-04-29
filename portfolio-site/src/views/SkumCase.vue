08548c629d3e7e08b147b8edf4e9a385554ce13a
wagnam <szma001@student.kristiania.no>
2022-04-29 23:21:34 +0200

    Almost finished skum case

--- portfolio-site/src/views/SkumCase.vue
+++ portfolio-site/src/views/SkumCase.vue
@@ -40,8 +40,233 @@
     <article class="the-process-container mt-5">
         <h3 class="fs-4" style="font-weight: 700">THE PROCESS</h3>
         <img class="img-fluid mt-5" src="../assets/images/UUU-model.png" alt="UUU model">
-
     </article>
+
+    <!-- Idea -->
+    <article class="row mt-4">
+      <div class="col-12">
+        <h3 class="fs-4" style="font-weight: 700;">EXPLORE</h3>
+        <div class="headning-underline"></div>
+      </div>
+      <div class="col-12 col-md-8 mt-3">
+        <h5 class="mt-5" style="font-weight: 700;">IDEA</h5>
+        <p class="about-projext-txt mt-4">The <strong>process</strong> started with our group sat down and wrote down every idea that came to mind.
+            It was <strong>important</strong> that everyone in our group was happy, so one of our members suggested an 
+            <strong>elimination technique.</strong> Everyone received a number of votes each round, until we were left with only one idea.</p>
+      </div>
+
+      <div class="col-12">
+        <img class="img-fluid" src="../assets/images/ide-process.png" alt="">
+      </div>
+    </article>
+
+    <!-- research -->
+
+    <article class="row mt-5">
+      <div class="col-12">
+        <h3 class="fs-5 mt-5" style="font-weight: 700;">RESEARCH</h3>
+      </div>
+      <div class="col-12 col-md-8">
+        <p style="font-weight: 700;" class="mt-5">Competitor Analysis</p>
+        <p class="about-projext-txt">To get a better understanding of the competitor and the beer market, I conducted <strong>analsyse</strong> on 
+                  one of the most popular beer/drink application on the market, and found that they provide a lot of information about beer and
+                  that they operate <strong>worldwide</strong>. This can be <strong>overwhelming</strong> for some users, especially those living in Norway.</p>
+        <p class="about-projext-txt">The analysis also provided us inspiration to some important <strong>key</strong> functions we should implement in our solution.
+            We came up with one primary target group.</p>
+        <h3 class="mt-5 target-group fs-4">“Beer enthusiasts in Norway between the ages of 30-50 years. Typical for these poeple is that they have a stable salary and can afford to buy finer and more luxurious beer. 
+            They are proud of the knowledge they have about beer and would love to share it with other people.”</h3>
+      </div>
+    </article>
+
+    <!-- User scenarios -->
+    <article class="row mt-5">
+      <div class="col-12">
+        <h3 class="fs-5 mt-5" style="font-weight: 700;">USER-SCENARIOS</h3>
+      </div>
+      <div class="col-12 col-md-8">
+        <p class="about-projext-txt mt-3">We created user-scenarios that we could use to better understand the <strong>potentional</strong> needs of beer enthusiasts and
+            corporate needs. This also helped us get a wider understanding of  the <strong>functional requirements</strong> for Skum.</p>
+      </div>
+    </article>
+
+    <div class="container px-4 mt-5 pb-5">
+        <div class="row gx-5">
+          <div class="col-12 col-md-6 ">
+            <div class="p-3 perspective-container">
+              <h4 class="text-center py-2 fs-4" style="font-weight: 700;">User Perspective</h4>
+              <p class="col-12 mt-5 primary-txt-color"> <i class=" bi bi-person-badge fs-4"></i>  As a beer-enthusiast I wish to discover new beer, 
+                                    so I can taste something new. </p>
+              <p class="col-12 my-5 primary-txt-color"> <i class=" bi bi-person-badge fs-4"></i>  As a beer-enthusiast I wish to discover new beer, 
+                                    so I can taste something new. </p>
+              <p class="col-12  primary-txt-color"> <i class=" bi bi-person-badge fs-4"></i>  As a beer-enthusiast I wish to discover new beer, 
+                                    so I can taste something new. </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 mt-5 mt-md-0">
+          <div class="p-3 perspective-container">
+            <h4 class="text-center py-2 fs-4" style="font-weight: 700;">Corporate Perspective</h4>
+            <p class="col-12 mt-5 primary-txt-color"> <i class=" bi bi-building fs-4"></i>  As a designer i wish to develop an intuitive navigation
+                    so the solution is easy to use for the customers. </p>
+            <p class="col-12 my-5 primary-txt-color"> <i class=" bi bi-building fs-4"></i>  As a designer i wish to develop an intuitive navigation
+                    so the solution is easy to use for the customers. </p>
+            <p class="col-12  primary-txt-color"> <i class=" bi bi-building fs-4"></i>  As a designer i wish to develop an intuitive navigation
+                    so the solution is easy to use for the customers. </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  <!-- End of user- scenarios -->
+
+  <!-- Conceptual model -->
+  <article class="row pb-5">
+    <div class="col-12">
+        <h3 class="fs-5 mt-5" style="font-weight: 700;">CONCEPTUAL MODEL</h3>
+      </div>
+      <div class="col-12 col-md-8 mt-4">
+        <p class="about-projext-txt">
+          After we created user-scenarios, the next step was to create a <strong>conseptual model.</strong> This model helped us <strong>visualize</strong> and 
+                        understand <strong>important</strong> elements of our solution and how we should <strong>organize</strong> the information architecture.
+        </p>
+      </div>
+
+      <div class="col-12">
+        <img class="img-fluid" src="../assets/images/consept-model.png" alt="conceptual model">
+      </div>
+  </article>
+
+  <!-- Sitemap -->
+  <article class="row pb-5">
+    <div class="col-12">
+        <h3 class="fs-5 mt-5" style="font-weight: 700;">SITEMAP</h3>
+    </div>
+
+    <div class="col-12 col-md-8 mt-4">
+        <p class="about-projext-txt">
+          For a better <strong>visualization</strong>, we organized the written post-it notes and created a sitemap. It provided us information 
+          about the <strong>pages, categories</strong> and the <strong>relationship</strong> between them.
+        </p>
+    </div>
+
+    <div class="col-12">
+      <img class="img-fluid" src="../assets/images/sitemap.png" alt="sitemap">
+    </div>
+  </article>
+
+  <!-- Design section -->
+  <article class="row">
+    <div class="col-12">
+        <h3 class="fs-4" style="font-weight: 700;">DESIGN</h3>
+        <div class="headning-underline-design"></div>
+      </div>
+  </article>
+
+  <!-- Low fidelity -->
+  <article class="row">
+    <div class="col-12">
+      <h3 class="fs-5 mt-5" style="font-weight: 600;">Low Fidelity Wireframes</h3>
+    </div>
+    <div class="col-12 col-md-8">
+       <p class="about-projext-txt mt-3">
+          Once we created the conceptual model and the sitemap, the next step was to start <strong>wireframing.</strong>
+          We started with pen and paper wireframes, where i focused on the <strong>group</strong> and <strong>event page,</strong> and created 
+          different versions of it to start with. 
+        </p>
+    </div>
+
+    <div class="container px-4 mt-5 pb-5">
+        <div class="row gx-5">
+          <div class="col-12 col-md-6 ">
+            <img class="img-fluid" src="../assets/images/low-f-1.png" alt="">
+        </div>
+        <div class="col-12 col-md-6 mt-5 mt-md-0">
+          <img class="img-fluid" src="../assets/images/low-f-2.png" alt="">
+        </div>
+      </div>
+    </div>
+  </article>
+
+  <!-- High fidelity -->
+   <article class="row mt-5">
+    <div class="col-12">
+      <h3 class="fs-5 mt-5" style="font-weight: 600;">High Fidelity Wireframes</h3>
+    </div>
+    <div class="col-12 col-md-8 mb-5">
+        <p class="about-projext-txt mt-3">
+          Next we created <strong>mid-fidelity</strong> and <strong>high-fidelity</strong> versions of these wireframes in Figma, created a <strong>clickable
+          prototype</strong> and found four people in our target group to test them out.   
+        </p>
+    </div>
+    <div class="col-12 ">
+        <img class="img-fluid" src="../assets/images/high-w.png" alt="">
+    </div>
+  </article>
+
+  <!-- TEST SECTION -->
+  <article class="row mt-5">
+    <div class="col-12">
+        <h3 class="fs-4" style="font-weight: 700;">TEST</h3>
+        <div class="headning-underline-test"></div>
+      </div>
+      <div class="col-12 col-md-8 mt-4">
+        <p class="about-projext-txt mt-3">
+          This phase was really important, by conducting <strong>usability test</strong>, we were able to get insight in 
+          <strong>user needs</strong> and discovered <strong>deficiencies</strong> we had not thought of. The useres were given a <strong>scenario-based tasks</strong> 
+          that would test the main functions of the application. After the tasks was finished we asked 
+          them several <strong>follow-up questions,</strong> including about how they feel about the app in general. All of the tests were
+          recorded and all results were put into a table and analyzed accordingly.    
+        </p>
+      </div>
+      <div class="col-12 mt-4 pt-5 text-center">
+        <h3 class="fs-5" style="font-weight: 600;">Key Findings</h3>
+      </div>
+  </article>
+
+  <div class="container px-4 mt-5 pb-5">
+        <div class="row gx-5">
+          <div class="col-12 col-md-6 ">
+            <div class="p-3 perspective-container">
+              <h4 class="text-center py-2 fs-4" style="font-weight: 700;">Positive</h4>
+              <p class="col-12 mt-5 primary-txt-color"> <i class=" bi bi-hand-thumbs-up fs-4"></i>  Users were able to complete tasks quickly.</p>
+              <p class="col-12 my-5 primary-txt-color"> <i class=" bi bi-hand-thumbs-up fs-4"></i>  Navigation proved to be easy.</p>
+              <p class="col-12  primary-txt-color"> <i class=" bi bi-hand-thumbs-up fs-4"></i>  Users used the word intuitive several times. </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 mt-5 mt-md-0">
+          <div class="p-3 perspective-container">
+            <h4 class="text-center py-2 fs-4" style="font-weight: 700;">Negative</h4>
+            <p class="col-12 mt-5 primary-txt-color"> <i class=" bi bi-hand-thumbs-down fs-4"></i>  Users was confused about groups and events. </p>
+            <p class="col-12 my-4 primary-txt-color"> <i class=" bi bi-hand-thumbs-down fs-4"></i>  50% of users were confused about the add button on the category page. </p>
+            <p class="col-12  primary-txt-color"> <i class=" bi bi-hand-thumbs-down fs-4"></i>  Users perceived that the existing edit button could only delete the list itself or add a new list. </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  <!-- Retrospective -->
+
+  <article class="row pt-5 mt-5">
+    <div class="col-12">
+      <h3 class="fs-4" style="font-weight: 700;">Retrospective</h3>
+      <h3 class="fs-5 mt-5" style="font-weight: 700;">Challenges</h3>
+    </div>
+
+    <div class="col-12 col-md-9 mt-5">
+      <p class="about-projext-txt">This was my first UX gorup project and <strong>working with others</strong> may be difficult sometimes, everyone has
+        their own ideas and sometime it my be hard to let them go. One of our <strong>first challeges</strong> was deciding on how
+        we sould <strong>communicate</strong> and <strong>collaborate</strong> with each other.  We decided to meet several times a week and talk 
+        through our toughts and ideas, and we made use of <strong>Discord</strong> to communicate when we were at home. </p>
+    </div>
+    <div class="col-12 col-md-9 mt-5">
+      <p class="about-projext-txt">In the <strong>beginning</strong> we spent much time  deciding ideas- until one of the group members suggested a elimination technique. <strong>Nobody 
+        took charge</strong> and we spent unnecessary much time on unimportant talking and doing other things. We also spent a lot of time working together instead
+        of <strong>allocating</strong> work tasks.  </p>
+    </div>
+    <div class="col-12 col-md-9 mt-5">
+      <p class="about-projext-txt">In some <strong>parts</strong> of the process, it was <strong>difficult</strong> to keep the <strong>user</strong> in mind, and we forgot that 
+                  we designed for the users and not ourselves.  </p>
+    </div>
+
+  </article>
 
   </main>
 
@@ -65,11 +290,70 @@ export default {
   font-family: Montserrat, sans-serif;
 }
 
+.bi-building{
+  color: #ECAE12 !important;
+}
+
+.bi-person-badge{
+  color: #ECAE12 !important;
+}
+.bi-hand-thumbs-down{
+  color: #ECAE12 !important;
+}
+.bi-hand-thumbs-up{
+  color: #ECAE12 !important;
+}
+
+.primary-txt-color{
+  color: #0d0d0d;
+}
+
+.perspective-container{
+  background-color: #F0F1E9;
+  border-radius: 5px;
+}
+
+.target-group{
+  color: rgba(13, 13, 13, 0.69) !important;
+  font-weight: 200;
+}
+
+.headning-underline{
+  background-color: #ECAE12;
+  height: 5px;
+  width: 7rem;
+}
+.headning-underline-design{
+  background-color: #ECAE12;
+  height: 5px;
+  width: 6rem;
+}
+.headning-underline-test{
+  background-color: #ECAE12;
+  height: 5px;
+  width: 4rem;
+}
+
+@media screen and (max-width: 425px){
+  .headning-underline{
+    width: 6.5rem;
+  }
+}
+@media screen and (max-width: 425px){
+  .headning-underline-design{
+    width: 5.5rem;
+  }
+}
+@media screen and (max-width: 425px){
+  .headning-underline-test{
+    width: 3.5rem;
+  }
+}
+
 .fs-4{
-    color: #0d0d0d;
+  color: #0d0d0d;
 }
 .about-project {
-  color: #0d0d0d;
   font-weight: 700;
 }
 
