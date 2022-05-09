ad3fba1eb2f7316afd97dd4bb49bfbc27fe8bbd7
wagnam <szma001@student.kristiania.no>
2022-05-09 19:16:45 +0200

    Finished portfolio

--- portfolio-site/src/views/AboutView.vue
+++ portfolio-site/src/views/AboutView.vue
@@ -18,7 +18,13 @@
 
         <div class="row mt-5 pt-5">
             <article class="col-12 col-md-8">
-                <p class="fs-4">I am an adventurous guy who loves learning new things. I've been backpacking in South-America for 4 months, 
+                <p class="fs-5">I am an adventurous guy who loves learning new things and always ready to face new challenges. 
+                    I am curious, spontaneous and always happy. 
+                    In a group, I am the person who asks a lot questions to understand the challenges we facing 
+                </p>
+            </article>
+            <article class="col-12 col-md-8 mt-3">
+                <p class="fs-5">I've been backpacking in South-America for 4 months, 
                         and that gave me a new perspective on a lot of things. I do a lot of sports activities in my sparetime, 
                         but my broken knee had made it a bit difficult the last two years.  </p>
             </article>
@@ -26,24 +32,79 @@
         
         <div class="row mt-5 align-middle pt-5">
             <article class="col-12 col-md-6">
-                <h2 class="text-center">Education</h2>
-                <h4 class="mt-5"><i class="bi bi-bank"></i> Høyskolen</h4>
+                <h2 class="" style="font-weight:500">Education</h2>
+                
+                <div class="row mt-5">
+                    <div class="col-12 col-md-2">
+                        <i class="bi bi-bank fs-1 "></i>
+                    </div>
+                    <div class="col-12 col-md-8">
+                        <p class="education-p-tag">AUG 2020-PRESENT</p>
+                        <h3>IT - Interactive Design</h3>
+                        <p class="education-p-tag">Bachelor - UX Design</p>
+                    </div>
+                </div>
+
             </article>
+
             <article class="col-12 col-md-6">
-                <h2 class="text-center">Experience</h2>
+                <h2 class="mt-4 mt-md-0" style="font-weight:500">Skills</h2>
+                <div class="row mt-5">
+                    <div class="col-12 col-md-3">
+                        <svg height="60" viewBox="0 0 200 300" width="100" xmlns="http://www.w3.org/2000/svg"><path d="M50 300c27.6 0 50-22.4 50-50v-50H50c-27.6 0-50 22.4-50 50s22.4 50 50 50z" fill="#0acf83"/><path d="M0 150c0-27.6 22.4-50 50-50h50v100H50c-27.6 0-50-22.4-50-50z" fill="#a259ff"/><path d="M0 50C0 22.4 22.4 0 50 0h50v100H50C22.4 100 0 77.6 0 50z" fill="#f24e1e"/><path d="M100 0h50c27.6 0 50 22.4 50 50s-22.4 50-50 50h-50z" fill="#ff7262"/><path d="M200 150c0 27.6-22.4 50-50 50s-50-22.4-50-50 22.4-50 50-50 50 22.4 50 50z" fill="#1abcfe"/></svg>
+                    </div>
+                    <div class="col-12 col-md-8 mt-3 mt-md-0">
+                        <h4>Figma</h4>
+                        
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-12 col-md-3">
+                        <svg class="" viewBox="0 0 240 234" xmlns="http://www.w3.org/2000/svg" width="100" height="60"><path d="M42.5 0h155C221 0 240 19 240 42.5v149c0 23.5-19 42.5-42.5 42.5h-155C19 234 0 215 0 191.5v-149C0 19 19 0 42.5 0z" fill="#001e36"/><g fill="#31a8ff"><path d="M54 164.1V61.2c0-.7.3-1.1 1-1.1 1.7 0 3.3 0 5.6-.1 2.4-.1 4.9-.1 7.6-.2s5.6-.1 8.7-.2 6.1-.1 9.1-.1c8.2 0 15 1 20.6 3.1 5 1.7 9.6 4.5 13.4 8.2 3.2 3.2 5.7 7.1 7.3 11.4 1.5 4.2 2.3 8.5 2.3 13 0 8.6-2 15.7-6 21.3s-9.6 9.8-16.1 12.2c-6.8 2.5-14.3 3.4-22.5 3.4-2.4 0-4 0-5-.1s-2.4-.1-4.3-.1V164c.1.7-.4 1.3-1.1 1.4H55.2c-.8 0-1.2-.4-1.2-1.3zm21.8-84.7V113c1.4.1 2.7.2 3.9.2H85c3.9 0 7.8-.6 11.5-1.8 3.2-.9 6-2.8 8.2-5.3 2.1-2.5 3.1-5.9 3.1-10.3.1-3.1-.7-6.2-2.3-8.9-1.7-2.6-4.1-4.6-7-5.7-3.7-1.5-7.7-2.1-11.8-2-2.6 0-4.9 0-6.8.1-2-.1-3.4 0-4.1.1zM192 106.9c-3-1.6-6.2-2.7-9.6-3.4-3.7-.8-7.4-1.3-11.2-1.3-2-.1-4.1.2-6 .7-1.3.3-2.4 1-3.1 2-.5.8-.8 1.8-.8 2.7s.4 1.8 1 2.6c.9 1.1 2.1 2 3.4 2.7 2.3 1.2 4.7 2.3 7.1 3.3 5.4 1.8 10.6 4.3 15.4 7.3 3.3 2.1 6 4.9 7.9 8.3 1.6 3.2 2.4 6.7 2.3 10.3.1 4.7-1.3 9.4-3.9 13.3-2.8 4-6.7 7.1-11.2 8.9-4.9 2.1-10.9 3.2-18.1 3.2-4.6 0-9.1-.4-13.6-1.3-3.5-.6-7-1.7-10.2-3.2-.7-.4-1.2-1.1-1.1-1.9v-17.4c0-.3.1-.7.4-.9s.6-.1.9.1c3.9 2.3 8 3.9 12.4 4.9 3.8 1 7.8 1.5 11.8 1.5 3.8 0 6.5-.5 8.3-1.4 1.6-.7 2.7-2.4 2.7-4.2 0-1.4-.8-2.7-2.4-4s-4.9-2.8-9.8-4.7c-5.1-1.8-9.8-4.2-14.2-7.2-3.1-2.2-5.7-5.1-7.6-8.5-1.6-3.2-2.4-6.7-2.3-10.2 0-4.3 1.2-8.4 3.4-12.1 2.5-4 6.2-7.2 10.5-9.2 4.7-2.4 10.6-3.5 17.7-3.5 4.1 0 8.3.3 12.4.9 3 .4 5.9 1.2 8.6 2.3.4.1.8.5 1 .9.1.4.2.8.2 1.2v16.3c0 .4-.2.8-.5 1-.9.2-1.4.2-1.8 0z"/></g></svg>
+                    </div>
+                    <div class="col-12 col-md-8  mt-md-0">
+                        <h4 class="mt-3 mt-md-0">Photoshop</h4>
+                        
+                    </div>
+                </div>
             </article>
         </div>
+
+        <div class="col-12 pt-5 my-5 text-center">
+            <router-link to="/contact-view">
+                <button class="btn contact-btn">CONTACT ME</button>
+            </router-link>
+        </div>
+
     </div>
 
  
 </template>
+
+<script>
+//import FigmaSvg from '../assets/images/figma-1.svg'
+export default {
+    components: {
+  //      FigmaSvg
+    }
+}
+</script>
 
 <style scoped>
 
 .row{
   font-family: Montserrat, sans-serif;
 }
+.contact-btn{
+    background-color: #EFA90B;
+    font-weight: 600;
+    min-width: 15rem;
+    font-size: 18px;
+}
 
+.education-p-tag{
+    color: rgba(21, 49, 54, .5);
+}
 .welcomeTxt{
     font-weight: 600;
     color: #153136;
---
+++ portfolio-site/src/views/ContactView.vue
@@ -0,0 +1,42 @@
+<template>
+    <section class="container">
+        <div class="row pt-5">
+            <article class="col-12 col-md-8">
+                
+                <img class="img-fluid mt-5" src="../assets/images/croods.png" alt="">
+            </article>
+            <article class="col-12 col-md-4 mt-5">
+                <h2 class="mb-5" style="font-weight: 600;">Get in touch</h2>
+                <h4>Email:</h4>
+                <a class="fs-4 nav-link p-0" href="mailto:szymon_98@icloud.com?Subject=Get%20In%20touch" target="_top" >szymon_98@icloud.com</a>
+                <h4 class="mt-5">By:
+                    <span>
+                        <a class="fs-4 mt-3 nav-link p-0" href="https://www.linkedin.com/in/szymon-mazur-07554b15b/">LinkedIn</a>
+                    </span>
+                    
+                    </h4>              
+        
+                
+                
+            </article>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.container{
+  font-family: Montserrat, sans-serif;
+}
+
+.nav-link{
+    color: rgba(21, 49, 54, 1);
+    font-weight: 500;
+}
+
+.nav-link:hover{
+    color: #EFA90B;
+}
+.img{
+    height: 50rem;
+}
+</style>
